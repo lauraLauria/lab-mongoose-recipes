f55dc937d50d9be121acd467a2bfa9637c983fe7
Laura Lauria <lauralauria@Air-von-Laura.local>
2023-11-10 18:50:46 +0100

    done

--- index.js
+++ index.js
@@ -16,8 +16,42 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title: "Bollognese",
+      level: "Easy",
+      ingredients: ["Sellerie", "Karotten", "Hack", "Tomaten"],
+      cuisine: "Italian",
+      dishType: "Main,
+      image:
+        "https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_960,c_limit/bolognese-1.jpg",
+      duration: 5,
+      creator: "unknown",
+    };
+
+    return Recipe.create(newRecipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  .then((result) => console.log(`Recipe created: ${result}`))
+  .then(() => Recipe.insertMany(data))
+  .then((result) => {
+    // console.log(`Created recipes:`, result);
+    console.log(`Created ${result.length} recipes`);
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 },
+      { new: true }
+    );
+  })
+  .then((result) => {
+    console.log(
+      `Updated ${result.title} and new duration is: ${result.duration}`
+    );
+
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((result) => {
+    console.log("The recipe was deleted", result);
+  })
+  .catch((error) => {
+    console.error("Error: ", error);
+  })
+  .finally(() => mongoose.connection.close());
